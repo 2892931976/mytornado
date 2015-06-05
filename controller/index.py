5de0b6c028da78b06ec6048817a9a0bead09bbe1
benlightning <394849890@qq.com>
2015-06-05 17:35:41 +0800

    clear some code

--- controller/index.py
+++ controller/index.py
@@ -8,4 +8,4 @@ import sys
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         #lst = ["python","http://www.cnblogs.com/benlightning/","benlightning@gmail.com"]
-        self.render("index.html")
+        self.render("public/500.html")
